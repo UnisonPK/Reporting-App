665c2a74825219105b85678691389ac70c683b2a
Muneeb Shehzad Butt <156073014+muneebbutt78@users.noreply.github.com>
2026-09-12 21:52:25 +0500

    Load Programme readiness dependency control

--- app-config.js
+++ app-config.js
@@ -16,6 +16,7 @@ window.addEventListener("load", function () {
   addScript("programmeControlEnhancementScript","programme-control-enhancement.js?v=20260912-2");
   addScript("programmeScheduleImportScript","programme-schedule-import.js?v=20260912-2");
   addScript("programmeDelayLookAheadScript","programme-delay-lookahead.js?v=20260912-1");
+  addScript("programmeReadinessControlScript","programme-readiness-control.js?v=20260912-1");
   addScript("designManagementStage1Script","design-management.js?v=20260830-1");
   addCss("designManagementUiCss","design-management-ui.css?v=20260830-3");
   addScript("designManagementUiScript","design-management-ui.js?v=20260830-2");
